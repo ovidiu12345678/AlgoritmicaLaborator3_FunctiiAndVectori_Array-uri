c49929700c7a71bcfff7cf134c5820373468f6eb
Ovidiu Anton Anton OVIDIUs <ovidiugabriel123456789@outlook.com>
2022-05-15 11:19:08 +0300

    Add project files.

---
+++ AlgoritmicaLaborator3_FunctiiAndVectori_Array-uri/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlgoritmicaLaborator3_FunctiiAndVectori_Array_uri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
